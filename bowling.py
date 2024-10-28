62afb599c3f53c7422ff979e7198507882bbb9d0
lottarantala <lottarantala20@gmail.com>
2024-10-28 18:48:57 +0200

    User story 9 - not completed

--- bowling.py
+++ bowling.py
@@ -6,7 +6,8 @@ class BowlingGame:
 
     def __init__(self):
         self.frames = []
-        self.bonus_throw = 0
+        self.first_bonus_throw = 0
+        self.second_bonus_throw = 0
     
     def add_frame(self, frame: Frame) -> None:
         if len(self.frames) >= 10:
@@ -23,19 +24,22 @@ class BowlingGame:
         for i, frame in enumerate(self.frames):
             if frame.is_spare():
                 if i == len(self.frames) - 1:
-                    frame.set_bonus(self.bonus_throw)
+                    frame.set_bonus(self.first_bonus_throw)
                 else:
                     frame.set_bonus(self.frames[i+1].get_first_throw())
             if frame.is_strike():
-                if self.frames[i+1].is_strike():
-                    frame.set_bonus(self.frames[i+1].get_first_throw() + self.frames[i+1].get_second_throw() + self.frames[i+2].get_first_throw())
+                if i == len(self.frames) - 1:
+                    frame.set_bonus(self.first_bonus_throw + self.second_bonus_throw)
                 else:
-                    frame.set_bonus(self.frames[i+1].get_first_throw() + self.frames[i+1].get_second_throw())
+                    if self.frames[i+1].is_strike():
+                        frame.set_bonus(self.frames[i+1].get_first_throw() + self.frames[i+1].get_second_throw() + self.frames[i+2].get_first_throw())
+                    else:
+                        frame.set_bonus(self.frames[i+1].get_first_throw() + self.frames[i+1].get_second_throw())
             score += frame.score()
         return score
 
     def set_first_bonus_throw(self, bonus_throw: int) -> None:
-        self.bonus_throw = bonus_throw
+        self.first_bonus_throw = bonus_throw
 
     def set_second_bonus_throw(self, bonus_throw: int) -> None:
-        pass
+        self.second_bonus_throw = bonus_throw
